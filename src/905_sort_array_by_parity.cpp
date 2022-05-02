faa9ac1fd6dea95a3666d74daa7384a280df787d
leeh8911@gmail.com <54252269+leeh8911@users.noreply.github.com>
2022-05-03 05:45:38 +0900

    905 solve! but its slow solution :(

---
+++ src/905_sort_array_by_parity.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [](int a, int b){return (a % 2) < (b%2);});
+        
+        return nums;
+    }
+};
+
+ostream& operator<<(ostream& os, vector<int> v){
+    os << "[";
+    for(auto elm : v){
+        os << elm << " ";
+    }
+    os << "]";
+    return os;
+}
+bool operator==(vector<int> a, vector<int> b){
+    if (a.size() == b.size()){
+        for(int i = 0; i < a.size(); i++){
+            if (a[i] != b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void printTest(Solution sol, vector<int> nums, vector<int> expect){
+    
+    vector<int> actual = sol.sortArrayByParity(nums);
+    if (actual == expect){
+        cout << "\033[32m" << "[PASS] " << "\033[0m" << nums << "\n";
+    }
+    else{
+        cout << "\033[31m" << "[FAIL] " << "\033[0m" << nums << "\n";
+        cout << "EXPECT : " << expect << "\n";
+        cout << "ACTUAL : " << actual << "\n";
+    }
+}
+
+int main(void){
+    Solution sol;
+
+    printTest(sol, {3,1,2,4}, {2,4,3,1});
+    printTest(sol, {0}, {0});
+
+    return 0;
+}
